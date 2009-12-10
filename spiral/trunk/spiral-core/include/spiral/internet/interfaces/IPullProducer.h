6f1c67f8a9712a5262fad720012d66c1788c9869
hitnrun <hitnrun@6cb40615-3296-4dc8-bcc4-269c24c2e8c1>
2009-12-10 21:00:22 +0000

    - Producer / Consumer implementation

---
+++ spiral/trunk/spiral-core/include/spiral/internet/interfaces/IPullProducer.h
@@ -0,0 +1,32 @@
+#ifndef _SPIRAL_INTERNET_INTERFACES_IPULLPRODUCER_H__
+#define _SPIRAL_INTERNET_INTERFACES_IPULLPRODUCER_H__
+
+#include "spiral/internet/interfaces/IProducer.h"
+
+namespace spiral {
+namespace internet {
+namespace interfaces {
+
+/**
+ *   A pull producer, also known as a non-streaming producer, is
+ *   expected to produce data each time resumeProducing() is called.
+ */
+class IPullProducer 
+{
+public:
+	/**
+     *   Produce data for the consumer a single time.
+     *
+     *   This tells a producer to produce data for the consumer once
+     *   (not repeatedly, once only). Typically this will be done
+     *   by calling the consumer's write() method a single time with
+     *   produced data.
+	 */
+	virtual void resumeProducing() = 0;
+
+	virtual ~IPullProducer() {}
+};
+
+}}} // spiral::internet::interfaces
+
+#endif // _SPIRAL_INTERNET_INTERFACES_IPULLPRODUCER_H__
---
+++ spiral/trunk/spiral-core/include/spiral/internet/interfaces/IPushProducer.h
@@ -0,0 +1,41 @@
+#ifndef _SPIRAL_INTERNET_INTERFACES_IPUSHPRODUCER_H__
+#define _SPIRAL_INTERNET_INTERFACES_IPUSHPRODUCER_H__
+
+#include "spiral/internet/interfaces/IProducer.h"
+
+namespace spiral {
+namespace internet {
+namespace interfaces {
+
+/**
+ *   A push producer, also known as a streaming producer is expected to
+ *   produce (write to this consumer) data on a continous basis, unless
+ *   it has been paused. A paused push producer will resume producing
+ *   after its resumeProducing() method is called.   For a push producer
+ *   which is not pauseable, these functions may be noops.
+ */
+class IPushProducer 
+{
+public:
+	/**
+     *   Pause producing data.
+	 *
+     *   Tells a producer that it has produced too much data to process for
+     *   the time being, and to stop until resumeProducing() is called.
+	 */
+	virtual void pauseProducing() = 0;
+
+	/**
+     *   Resume producing data.
+     *
+     *   This tells a producer to re-add itself to the main loop and produce
+     *   more data for its consumer.
+	 */
+	virtual void resumeProducing() = 0;
+
+	virtual ~IPushProducer() {}
+};
+
+}}} // spiral::internet::interfaces
+
+#endif // _SPIRAL_INTERNET_INTERFACES_IPUSHPRODUCER_H__
